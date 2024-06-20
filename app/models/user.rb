47380ad3209d42f2d7c69a76717efce1ae3f24bc
EC2 Default User <ec2-user@ip-172-31-10-197.ap-northeast-1.compute.internal>
2024-06-20 14:02:47 +0000

    検索機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,12 @@
 class User < ApplicationRecord
-  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :books
-  has_many :book_comments, dependent: :destroy
-  has_many :favorites, dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_of_relationships, source: :follower
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followed
-  
+  has_many :favorites
+  has_many :favorite_books, through: :favorites, source: :book
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -21,15 +17,15 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
   
-  def follow(user)
-    relationships.create(followed_id: user.id)
-  end
-
-  def unfollow(user)
-    relationships.find_by(followed_id: user.id).destroy
-  end
-
-  def following?(user)
-    followings.include?(user)
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
   end
 end
